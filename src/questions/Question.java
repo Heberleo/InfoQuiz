84dbc01202a37773e88bf1c0801f1a192b3df945
nikb99 <nik.biedert@googlemail.com>
2020-06-09 10:09:44 +0200

    Stats-Window zu JDialog gemacht und temporären questionSupplier eingefügt

--- src/questions/Question.java
+++ src/questions/Question.java
@@ -1,14 +1,11 @@
 package questions;
 
 public abstract class Question {
-	
-	public static final int FILL_BLANK = 1;
-	public static final int MULTIPLE_CHOICE = 2;
 
 	private Stats stats;
 	private String title;
 	private int time; // in seconds
-	protected int type;
+	protected Questiontype type;
 
 	public Question(String title, int time) {
 		setTitle(title);
@@ -26,7 +23,7 @@ public abstract class Question {
 		this.title = title;
 	}
 	
-	public int getType() {
+	public Questiontype getType() {
 		return this.type;
 	}
 	
@@ -35,7 +32,7 @@ public abstract class Question {
 	}
 	
 	public void setTime(int time) throws IllegalArgumentException {
-		if (this.time < 1)
+		if (time < 1)
 			throw new IllegalArgumentException("Time must be greater than 0 seconds!");
 		this.time = time;
 	}
